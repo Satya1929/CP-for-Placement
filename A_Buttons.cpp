fdd65bfa0af8d80a8d83684579a1f471320a0365
Satya1929 <spswain2003.org@gmail.com>
2025-03-07 21:30:18 +0530

    cp 7 march

---
+++ A_Buttons.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+int main()
+{
+    int t ;
+    cin >> t ;
+    while(t--)
+    {
+        int a,b,c ;
+        cin >> a >> b >> c ;
+
+        if (a>b) cout << "First" << endl ;
+        else if (a<b) cout << "Second" << endl ;
+        else // a==b
+        {
+            if(c%2 == 0) cout << "Second" << endl ;
+            else cout << "First" << endl ;
+        }
+    }
+
+    return 0 ;
+}
+
+
+
+
